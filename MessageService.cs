eb8dc632678cfafdabe049341457fab4c8c05f61
Kevin <kwcw@live.com>
2022-03-28 22:39:43 +1100

    New Delegate Test Folder

---
+++ MessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateTest
+{
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            System.Console.WriteLine("MessageService: Sending a test message..." + e.Video.Title);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateTest
+{
+
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+            var messageService = new MessageService(); // subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+
+            System.Console.ReadLine();
+        }
+    }
+
+}
