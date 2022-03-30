14d226da2d37d1a2f6af142809195c4d5ecd07c2
Kevin <kwcw@live.com>
2022-03-30 22:11:46 +1100

    Making it more understandable when printed out

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace DelegateTest
     {
         static void Main(string[] args)
         {
-            var video = new Video() { Title = "Video 1" };
+            var video = new Video() { Title = "Video 1" }; // New Video Instance with Property Title = Video 1
             var videoEncoder = new VideoEncoder(); // publisher
-            var mailService = new MailService(); // subscriber
+            var mailService = new mailService(); // subscriber
             var messageService = new MessageService(); // subscriber
 
             videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
@@ -24,5 +24,8 @@ namespace DelegateTest
             System.Console.ReadLine();
         }
     }
-
+    public class Video
+    {
+        public string Title { get; set; }
+    }
 }
--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -6,6 +6,7 @@ using System.Threading;
 
 namespace DelegateTest
 {
+    public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
     public class VideoEncoder
     {
 
@@ -13,24 +14,41 @@ namespace DelegateTest
         // 2- Define an event based on that delegate
         // 3- Raise the event
 
-
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event VideoEncodedEventHandler VideoEncoded; // Methods can be attached to this Event.
 
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video");
             Thread.Sleep(1500);
-            OnVideoEncoded(video);
+            PublisherOnVideoEncoded(video);
         }
 
-        protected virtual void OnVideoEncoded(Video video)
+        protected virtual void PublisherOnVideoEncoded(Video video) // Publisher Method
         {
-            if(VideoEncoded != null)
+            if (VideoEncoded != null) // Check if VideoEncoded Events have methods attached to it.
             {
                 VideoEncoded(this, new VideoEventArgs() { Video = video });
+                // If VideoEncoded Event have Methods attached to it,
+                // then make the VideoEncoded Event call the methods attached to it,
+                // with the following input --> Video = video
             }
         }
-
     }
+    public class mailService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e) // Method with Delegate Signature
+        {
+            Console.WriteLine("MailService: Sending an email..." + e.Video.Title);
+        }
+    }
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e) // Method with Delegate Signature
+        {
+            Console.WriteLine("MessageService: Sending an message..." + e.Video.Title);
+        }
+    }
+
 }
+
+
